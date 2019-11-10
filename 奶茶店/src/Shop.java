9d8db056aac291d748a3cdd67fd34001a72ecc7b
XydfLi <690054845@qq.com>
2019-11-10 13:12:18 +0800

    删除了奶茶店

--- 奶茶店/src/Shop.java
+++
@@ -1,5 +0,0 @@
-public interface Shop
-{
-    public abstract boolean addingredient(Ingredient ingredient);//进货方法,false为进货失败
-    public void sellmilktea(String milkname,String ingname) throws SoldOutException;//出售奶茶方法
-}
